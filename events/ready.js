a910ea5e2bf45431c7189a95de3c7c39405190e4
restia1230 <ericlin1230@hotmail.com>
2020-07-18 17:42:23 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports = (client) => {
-        client.user.setActivity(`?help`);
+        client.user.setActivity(`?help / ?helpdm`);
         console.log(`Ready to serve on ${client.guilds.cache.size} servers, for ${client.users.cache.size} users.`);
 } 
